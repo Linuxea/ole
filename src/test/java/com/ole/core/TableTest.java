45c5968bc3f0cd2a3c4658f5dfaee5b09c05b88c
linuxea <linuxea@foxmail.com>
2017-08-21 14:59:16 +0800

    add a demo test

---
+++ src/test/java/com/ole/core/TableTest.java
@@ -0,0 +1,15 @@
+package com.ole.core;
+
+import org.junit.Test;
+
+/**
+ * Created by Linuxea on 2017/8/21.
+ */
+public class TableTest {
+
+	@Test
+	public void test1(){
+		System.out.println("hello!ole");
+	}
+
+}
